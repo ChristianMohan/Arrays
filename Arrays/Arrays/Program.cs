9a51deb4ac7a119f0c02cb69a60e3674687eaf7e
ChristianMohan <christianjmohan@gmail.com>
2017-10-17 12:32:35 -0400

    Arrays v 0.1
    finished 1

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double[] array = new double[100];
+            string[] array1 = new string[100];
+
+
+             for (int i = 0; i < 100; i++)
+            {
+                array[i] = i; 
+                array1[i] = "";
+                if (array[i] % 3 == 0)
+                {
+                    array1[i] = array1[i] + "fizz"; 
+                }
+                if (array[i] % 5 == 0)
+                {
+                    array1[i] = array1[i] + "buzz";
+                }
+                Console.WriteLine(array[i] + " . " + array1[i]);
+            }
+            Console.WriteLine("hi");
+
+        }
+    }
+}
---
+++ Arrays/unique digits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unique_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[51];
+            Random random = new Random();
+            
+           for (int i = 1; i < 51; i++)
+            {
+                int randomNum = random.Next(1, 50);
+                array[i] = randomNum;
+                if( )
+                {
+                    Console.WriteLine(array[i]);
+                }
+
+            }
+            Console.WriteLine("");
+        }
+    }
+}
