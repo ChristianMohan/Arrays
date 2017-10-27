b151730a8025fe2cd29edb03d5efb468f0a7ef53
ChristianMohan <christianjmohan@gmail.com>
2017-10-27 12:53:18 -0400

    changed
    eatshsrthshrt

--- Arrays/Dedupe/Program.cs
+++ Arrays/Dedupe/Program.cs
@@ -12,12 +12,20 @@ namespace Dedupe
         {
             int[] array = new int[5000];
             Random random = new Random();
-            int i;
-            for(i = 0; i < array.Length; i++)
+            int i = 0;
+            int p;
+
+            for(p = 0; p< array.Length; p++)
             {
                 int randomNum = random.Next(1, 50);
                 array[i] = randomNum;
-                Array.Sort(array);
+                i++;
+            }
+
+            for(i = 0; i < array.Length; i++)
+            {
+
+                
 
                 for (int j = i + 1; j <array.Length ; j++)
                 {
@@ -25,13 +33,14 @@ namespace Dedupe
                     {
                         array[i] = -1;
                     }
-                    Console.Write(array[i] + " ");
+                    
                 }
 
 
-
+                Console.Write(array[i] + " ");
+                
             }
-
+            
 
 
 
